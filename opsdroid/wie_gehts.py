3c6191c43f21825b4f48d2651f2fd5e485f7044a
42FordPrefect <webcrap@mailbox.org>
2022-01-25 22:15:40 +0100

    fix await

--- opsdroid/wie_gehts.py
+++ opsdroid/wie_gehts.py
@@ -19,16 +19,17 @@ class HelloByeSkill(Skill):
 
     @match_regex(r'wie geht', case_sensitive=False)
     async def wie_gehts(self, message):
-        msg1 = subscribe.simple("paula/erde", hostname="localhost").payload.decode('utf-8')
+        await msg1 = subscribe.simple("paula/erde",hostname="localhost").payload.decode('utf-8')
         await message.respond(msg1)
 
 
     @match_regex(r'wie warm', case_sensitive=False)
     async def wie_warm(self, message):
-        await message.respond(subscribe.simple("paula/temp", hostname="localhost").payload.decode('utf-8'))
+        await msg2 = subscribe.simple("paula/temp", hostname="localhost").payload.decode('utf-8')
+        await message.respond(msg2)
 
 
     @match_regex(r'Luftfeuchtigkeit', case_sensitive=False)
     async def wie_humid(self, message):
-        msg3 = subscribe.simple("paula/luftfeuchtigkeit", hostname="localhost").payload.decode('utf-8')
+        await msg3 = subscribe.simple("paula/luftfeuchtigkeit", hostname="localhost").payload.decode('utf-8')
         await message.respond(msg3)
